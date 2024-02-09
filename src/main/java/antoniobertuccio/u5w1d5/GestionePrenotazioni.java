38b2fa7279281d04756a6be433ccc402fd376c89
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-09 17:36:12 +0100

    Rename U5W1D5Application.java to GestionePrenotazioni.java in src/main/java/antoniobertuccio/u5w1d5.

--- src/main/java/antoniobertuccio/u5w1d5/GestionePrenotazioni.java
+++ src/main/java/antoniobertuccio/u5w1d5/GestionePrenotazioni.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class U5W1D5Application {
+public class GestionePrenotazioni {
 
 	public static void main(String[] args) {
-		SpringApplication.run(U5W1D5Application.class, args);
+		SpringApplication.run(GestionePrenotazioni.class, args);
 	}
 
 }
